4b212920b8082006187b9ad3811310d4ac779b66
ice <ice@lab06m27.nrc.ice.ufjf.br>
2017-08-24 22:01:18 -0300

    Final

---
+++ src/br/ufjf/dcc171/Aula06Exm01.java
@@ -0,0 +1,15 @@
+package br.ufjf.dcc171;
+
+import javax.swing.JFrame;
+
+public class Aula06Exm01 {
+
+    public static void main(String[] args) {
+        JanelaRadio janela = new JanelaRadio();
+        janela.setSize(300, 200);
+        janela.setLocationRelativeTo(null);
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        janela.setVisible(true);
+    }
+    
+}
